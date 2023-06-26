ee2dae6d922643dbd6f1367c4e56645b61968aba
Nile Okomo <okomonile3@gmail.com>
2023-06-26 23:42:19 +0300

    Add prototype for insertion sort algorithm

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -19,5 +20,6 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort(listint_t **list);
 
 #endif /* SORT_H */
